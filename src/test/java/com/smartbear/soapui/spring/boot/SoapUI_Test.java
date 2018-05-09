d2870ebf04951c036cad5daa55bc9e95a0eeb240
jeebiz <hnxyhcwdl1003@163.com>
2018-05-09 22:07:52 +0800

    x

--- src/test/java/com/smartbear/soapui/spring/boot/SoapUI_Test.java
+++ src/test/java/com/smartbear/soapui/spring/boot/SoapUI_Test.java
@@ -5,10 +5,16 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
+import org.apache.xmlbeans.SchemaType;
 import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.impl.schema.SchemaTypeImpl;
 import org.junit.Test;
 
+import com.eviware.soapui.config.RestServiceConfig;
+import com.eviware.soapui.config.impl.RestServiceConfigImpl;
 import com.eviware.soapui.impl.WsdlInterfaceFactory;
+import com.eviware.soapui.impl.rest.RestService;
+import com.eviware.soapui.impl.rest.RestServiceFactory;
 import com.eviware.soapui.impl.wsdl.WsdlInterface;
 import com.eviware.soapui.impl.wsdl.WsdlOperation;
 import com.eviware.soapui.impl.wsdl.WsdlProject;
@@ -22,7 +28,7 @@ import com.eviware.soapui.support.SoapUIException;
 public class SoapUI_Test {
 
 	@Test
-	public void test() throws XmlException, IOException, SoapUIException, SubmitException {
+	public void testSoap() throws XmlException, IOException, SoapUIException, SubmitException {
 		
 		// create new project
 		WsdlProject project = new WsdlProject();
@@ -53,5 +59,41 @@ public class SoapUI_Test {
 		assertTrue( content.indexOf( "404 Not Found" ) > 0  );
 		
 	}
+	
+	@Test
+	public void testRest() throws XmlException, IOException, SoapUIException, SubmitException {
+		
+		// create new project
+		WsdlProject project = new WsdlProject();
+		
+		RestService rest = new RestServiceFactory().createNew(project, "");
+		
+		// import amazon wsdl
+		WsdlInterface iface = WsdlInterfaceFactory.importWsdl( 
+		 project, "http://www.mycorp.com/somewsdl.wsdl", true )[0];
+
+		// get desired operation
+		WsdlOperation operation = (WsdlOperation) iface.getOperationByName( "MyOperation" );
+
+		// create a new empty request for that operation
+		WsdlRequest request = operation.addNewRequest( "My request" );
+
+		// generate the request content from the schema
+		request.setRequestContent( operation.createRequest( true ) );
+		
+		// submit the request
+		WsdlSubmit<WsdlRequest> submit = request.submit( new WsdlSubmitContext(request), false );
+
+		// wait for the response
+		Response response = submit.getResponse();
+		
+		// print the response
+		String content = response.getContentAsString();
+		System.out.println( content );
+		assertNotNull( content );
+		assertTrue( content.indexOf( "404 Not Found" ) > 0  );
+		
+	}
+	
 	
 }
