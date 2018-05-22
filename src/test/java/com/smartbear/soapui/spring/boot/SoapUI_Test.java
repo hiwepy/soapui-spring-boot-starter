90a3401ff8a61b1ab190a2f195f26a7292c57fe2
jeebiz <hnxyhcwdl1003@163.com>
2018-05-22 22:47:22 +0800

    learn

--- src/test/java/com/smartbear/soapui/spring/boot/SoapUI_Test.java
+++ src/test/java/com/smartbear/soapui/spring/boot/SoapUI_Test.java
@@ -5,34 +5,67 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
-import org.apache.xmlbeans.SchemaType;
 import org.apache.xmlbeans.XmlException;
-import org.apache.xmlbeans.impl.schema.SchemaTypeImpl;
 import org.junit.Test;
 
-import com.eviware.soapui.config.RestServiceConfig;
-import com.eviware.soapui.config.impl.RestServiceConfigImpl;
 import com.eviware.soapui.impl.WsdlInterfaceFactory;
+import com.eviware.soapui.impl.rest.RestRequest;
+import com.eviware.soapui.impl.rest.RestResource;
 import com.eviware.soapui.impl.rest.RestService;
 import com.eviware.soapui.impl.rest.RestServiceFactory;
+import com.eviware.soapui.impl.support.AbstractInterface;
+import com.eviware.soapui.impl.wsdl.InterfaceFactoryRegistry;
 import com.eviware.soapui.impl.wsdl.WsdlInterface;
 import com.eviware.soapui.impl.wsdl.WsdlOperation;
 import com.eviware.soapui.impl.wsdl.WsdlProject;
 import com.eviware.soapui.impl.wsdl.WsdlRequest;
 import com.eviware.soapui.impl.wsdl.WsdlSubmit;
 import com.eviware.soapui.impl.wsdl.WsdlSubmitContext;
+import com.eviware.soapui.model.environment.EnvironmentListener;
+import com.eviware.soapui.model.environment.Property;
 import com.eviware.soapui.model.iface.Request.SubmitException;
 import com.eviware.soapui.model.iface.Response;
+import com.eviware.soapui.model.project.Project;
+import com.eviware.soapui.model.support.ProjectListenerAdapter;
 import com.eviware.soapui.support.SoapUIException;
+import com.eviware.soapui.tools.SoapUIMockServiceRunner;
+import com.eviware.soapui.tools.SoapUITestCaseRunner;
 
 public class SoapUI_Test {
 
-	@Test
+	/*@Test
+    public void testCalculatorService() throws Exception {
+        SoapUITestCaseRunner testCaseRunner = new SoapUITestCaseRunner();
+        SoapUIMockServiceRunner mockServiceRunner = new SoapUIMockServiceRunner();
+
+        testCaseRunner.setProjectFile("src/test/resources/calculator-soapui-project.xml");
+        mockServiceRunner.setProjectFile("src/test/resources/calculator-soapui-project.xml");
+        mockServiceRunner.run();
+        testCaseRunner.run();
+	}*/
+	 
+	//@Test
 	public void testSoap() throws XmlException, IOException, SoapUIException, SubmitException {
 		
 		// create new project
 		WsdlProject project = new WsdlProject();
-
+		
+		project.addEnvironmentListener(new EnvironmentListener() {
+			
+			@Override
+			public void propertyValueChanged(Property property) {
+				System.out.println(property.getName() + ":" + property.getValue());
+			}
+		});
+		project.addProjectListener(new ProjectListenerAdapter() {
+			
+			@Override
+			public void afterLoad(Project project) {
+				super.afterLoad(project);
+			}
+			
+		});
+		
 		// import amazon wsdl
 		WsdlInterface iface = WsdlInterfaceFactory.importWsdl( 
 		 project, "http://www.webxml.com.cn/WebServices/IpAddressSearchWebService.asmx?wsdl", true )[0];
@@ -42,13 +75,16 @@ public class SoapUI_Test {
 		
 		// create a new empty request for that operation
 		WsdlRequest request = operation.addNewRequest( "My request" );
-
+		
 		// generate the request content from the schema
 		request.setRequestContent( operation.createRequest( true ) );
 		
+		WsdlSubmitContext context = new WsdlSubmitContext(request);
+		
+		context.put("", "127.0.0.1");
 		
 		// submit the request
-		WsdlSubmit<WsdlRequest> submit = request.submit( new WsdlSubmitContext(request), false );
+		WsdlSubmit<WsdlRequest> submit = request.submit( context, false );
 
 		// wait for the response
 		Response response = submit.getResponse();
@@ -57,34 +93,39 @@ public class SoapUI_Test {
 		String content = response.getContentAsString();
 		System.out.println( content );
 		assertNotNull( content );
-		assertTrue( content.indexOf( "404 Not Found" ) > 0  );
+		//assertTrue( content.indexOf( "404 Not Found" ) > 0  );
 		
 	}
-	
+	 
 	@Test
 	public void testRest() throws XmlException, IOException, SoapUIException, SubmitException {
 		
 		// create new project
 		WsdlProject project = new WsdlProject();
+		project.addEnvironmentListener(new EnvironmentListener() {
+			
+			@Override
+			public void propertyValueChanged(Property property) {
+				System.out.println(property.getName() + ":" + property.getValue());
+			}
+		});
+		project.addProjectListener(new ProjectListenerAdapter() {
+			
+			@Override
+			public void afterLoad(Project project) {
+				super.afterLoad(project);
+			}
+			
+		});
 		
-		RestService rest = new RestServiceFactory().createNew(project, "");
+		RestService rest = (RestService) InterfaceFactoryRegistry.createNew(project, RestServiceFactory.REST_TYPE, "test");
 		
-		// import amazon wsdl
-		WsdlInterface iface = WsdlInterfaceFactory.importWsdl( 
-		 project, "http://www.mycorp.com/somewsdl.wsdl", true )[0];
-
-		// get desired operation
-		WsdlOperation operation = (WsdlOperation) iface.getOperationByName( "MyOperation" );
-
-		// create a new empty request for that operation
-		WsdlRequest request = operation.addNewRequest( "My request" );
-
-		// generate the request content from the schema
-		request.setRequestContent( operation.createRequest( true ) );
 		
-		// submit the request
-		WsdlSubmit<WsdlRequest> submit = request.submit( new WsdlSubmitContext(request), false );
-
+		RestResource res = rest.getResourcesByFullPath("https://www.sojson.com/open/api/weather/json.shtml")[0];
+		 
+		res.getChildResourceList();
+		
+		 /*
 		// wait for the response
 		Response response = submit.getResponse();
 		
@@ -92,7 +133,7 @@ public class SoapUI_Test {
 		String content = response.getContentAsString();
 		System.out.println( content );
 		assertNotNull( content );
-		assertTrue( content.indexOf( "404 Not Found" ) > 0  );
+		assertTrue( content.indexOf( "404 Not Found" ) > 0  );*/
 		
 	}
 	
